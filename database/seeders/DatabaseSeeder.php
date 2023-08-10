28db98dbf5f2101edf05bacbbbea6b3e61c2a0e0
Enrico P. Chavez <vez3@yahoo.com>
2023-08-10 14:12:42 +0800

    create seeded for user and location

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,5 +28,6 @@ class DatabaseSeeder extends Seeder
        // Reservation::factory(25)->create();
        // Payment::factory(25)->create();
         Location::factory(10)->create();
+        User::factory(3)->create();
     }
 }
