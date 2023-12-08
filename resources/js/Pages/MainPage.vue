9d5325e5c97fd1b9e0de3f0757ccec6a60af383f
papiipapz <jessdrewpigg@gmail.com>
2023-12-08 23:56:30 +0800

    papicommit, dashboard

--- resources/js/Pages/MainPage.vue
+++ resources/js/Pages/MainPage.vue
@@ -15,24 +15,24 @@ defineProps({
 
 
 <template>
-<nav class="bg-gradient-to-b from-green-400 to-blue-400
-            fixed top-0 z-50 w-full bg-white border-b border-gray-200
-            dark:bg-gray-800 dark:border-gray-700">
+<nav class="bg-gradient-to-b from-black to-black
+            fixed top-0 z-50 w-full bg-white border-b border-black
+            dark:bg-gray-800 dark:border-gray-700 drop-shadow-md">
   <div class="px-3 py-3 lg:px-5 lg:pl-3">
         <div class="flex items-center justify-between">
                 <div class="flex items-center justify-start">
                     <Link href="/" class="flex ml-2 md:mr-24">
                     <img src="/partas_logo.png" class="h-8 mr-3" alt="FlowBite Logo" />
-                    <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Partas Bus Inc</span>
+                    <span class="self-center text-xl font-sans font-semibold sm:text-2xl whitespace-nowrap text-white">Partas Bus Inc</span>
                     </Link>
-                    <span class="self-center text-xl text-green-800 font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Ticket Reservation System</span>
+                    <span class="self-center text-xl font-sans text-white font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Ticket Reservation System</span>
                 </div>
                 <div class="flex items-center">
-                        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+                        <div v-if="canLogin" class="sm:absolute sm:top sm:transform sm:right-0 p-6 text-right">
                                 <Link
                                     v-if="$page.props.auth.user"
                                     :href="route('dashboard')"
-                                    class="font-semibold text-2xl text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                    class="font-semibold text-2xl text-white font-sans hover:text-red-500 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                                     >Dashboard</Link
                                 >
 
@@ -40,7 +40,7 @@ defineProps({
                                     <div class="flex justify-between">
                                     <Link
                                         :href="route('login')"
-                                        class="flex font-semibold  text-xl text-gray-600 hover:text-gray-900 dark:text-gray-400
+                                        class="flex font-semibold  text-xl text-white hover:text-red-500 dark:text-gray-400
                                                dark:hover:text-white focus:outline focus:outline-2
                                                focus:rounded-sm focus:outline-red-500"
                                         >
@@ -53,7 +53,7 @@ defineProps({
                                     <Link
                                         v-if="canRegister"
                                         :href="route('register')"
-                                        class="flex ml-4 font-semibold text-xl text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                        class="flex ml-4 font-semibold text-xl text-white hover:text-red-500 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                                         >
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                              <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
@@ -69,33 +69,68 @@ defineProps({
   </div>
 </nav>
 
-<div class="p-6 dark:border-gray-700 mt-14 bg-blue-400">
-    <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="/1.png" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="/2.png" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="/3.png" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="/4.png" alt="">
-        </div>
-        <div>
-            <Carousel />
-        </div>
-        <div>
-            <img class="carousel1.png" alt="">
+
+
+<div class="p-2 bg-white min-h-screen ">
+            
+<!---->
+<div class="flex justify-center mt-10">
+<div class="carousel rounded-lg w-full  h-96 mx-0 my-2.5 drop-shadow-2xl snap-center ">
+  <div id="item1" class="carousel-item relative w-full h-full  ">
+    <img src="/partas.jpg" class="w-full" />
+  </div> 
+  <div id="item2" class="carousel-item relative w-full h-full  ">
+    <img src="/partas1.jpg" class="w-full" />
+  </div> 
+  <div id="item3" class="carousel-item relative w-full h-full ">
+    <img src="/partas2.jpg" class="w-full" />
+  </div> 
+  <div id="item4" class="carousel-item relative w-full h-full ">
+    <img src="/partas.jpg" class="w-full" />
+  </div>
+</div> 
+</div>
+<div class="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" class="btn btn-xs text-white font-sans hover:text-red-500 ">o</a> 
+  <a href="#item2" class="btn btn-xs text-white font-sans hover:text-red-500 ">o</a> 
+  <a href="#item3" class="btn btn-xs text-white font-sans hover:text-red-500 ">o</a> 
+  <a href="#item1" class="btn btn-xs text-white font-sans hover:text-red-500 ">o</a>
+</div>
+        <!---->
+     
+    <div class="grid grid-cols-2 gap-4 mt-10 text-justify grid grid-flow-col hover:grid-flow-row">
+        <div class=" mt-10 text-xl font-serif text-black bg-white p-3 ">
+            <P>Partas Transportation Co., Inc. is a bus transportation company in the Philippines.
+                 It operates a 24/7 service for passengers and freight (known as "waybills") between Metro Manila and northern Luzon (the Ilocos Region and Cagayan Valley), with services also running to the Cordilleras in Baguio, Bangued in Abra, and also to the south Occidental Mindoro. Partas also offers chartered service for tour groups.</P>
+         
         </div>
 
+             
+        <div>
+            <img src="/partas2.jpeg" class="w-full" />
+        </div>
+      
     </div>
-</div>
+
+    </div>
+
+<footer class="footer p-10 bg-black text-neutral-content">
+  <aside>
+    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+    <p>Partas Inc.<br/>Providing reliable service since 1989</p>
+  </aside> 
+  <nav>
+    <header class="footer-title">Social</header> 
+    <div class="grid grid-flow-col gap-4">
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+    </div>
+  </nav>
+</footer>
+
+    
 
 </template>
 
-<style>
 
-
-</style>
